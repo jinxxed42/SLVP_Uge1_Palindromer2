e6d6de8911b6b412df77acf746cda8083eece20f
Claus Damgaard <jinxxed@outlook.com>
2023-03-21 20:52:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please input a string: ");
+String toCheck = Console.ReadLine();
+
+string[] ignoredChars = { " ", "_", "-", "!", "\"", "#", "¤", "%", "&", "/", "(", ")", "=", "+", "`", "\\", ",", "." };
+
+string toCheckTrimmed = toCheck;
+
+for (int i = 0; i < ignoredChars.Length; i++)
+{
+    toCheckTrimmed = toCheckTrimmed.Replace(ignoredChars[i], "");
+}
+
+
+char[] strArray = toCheckTrimmed.ToCharArray();
+
+Array.Reverse(strArray);
+
+String reverseString = new string(strArray);
+
+
+
+if (toCheckTrimmed.Equals(reverseString))
+{
+    Console.WriteLine(toCheckTrimmed + " is a palindrome.");
+}
+else
+{
+    Console.WriteLine(toCheckTrimmed + " is not a palindrome.");
+}
